2e4464d49c6f3ae8a6e13ded4dcb8567433a9838
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2021-12-31 10:57:04 +0530

    2

---
+++ src/main2.js
@@ -0,0 +1,4 @@
+import { add } from "./math2.js";
+
+let output = add(1, 1);
+console.log("Hello World", output);
---
+++ src/math2.js
@@ -0,0 +1,18 @@
+export function add(n1, n2) {
+  return n1 + n2;
+}
+
+export function minus(n1, n2) {
+  return n1 - n2;
+}
+
+export function mul(n1, n2) {
+  return n1 * n2;
+}
+
+export function divide(n1, n2) {
+  return n1 / n2;
+}
+
+// export {add};
+// export { add, minus, mul, divide };
