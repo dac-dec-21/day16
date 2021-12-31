d13b0fe8508b913cfa3f5a2bb7ea29bfadf9069e
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2021-12-31 11:09:55 +0530

    4

---
+++ src/main4.js
@@ -0,0 +1,4 @@
+import { add } from "./register/math4.js";
+
+let ouput = add(1, 1);
+console.log("Main4", ouput);
---
+++ src/register/math4.js
@@ -0,0 +1,4 @@
+export let add = (n1, n2) => n1 + n2;
+export let subtract = (n1, n2) => n1 - n2;
+export let mul = (n1, n2) => n1 * n2;
+export let divide = (n1, n2) => n1 / n2;
