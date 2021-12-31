b4b93d559e4b3341b1e4a16d8272d7d7db9fc075
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2021-12-31 11:03:26 +0530

    3

---
+++ src/main3.js
@@ -0,0 +1,4 @@
+import { add } from "./register/math3.js";
+
+const output = add(2, 2);
+console.log("MAIN 3", output);
---
+++ src/register/math3.js
@@ -0,0 +1,8 @@
+let add = (n1, n2) => n1 + n2;
+let subtract = (n1, n2) => n1 - n2;
+export let mul = (n1, n2) => n1 * n2;
+export let divide = (n1, n2) => n1 / n2;
+
+export { add };
+export { subtract };
+//export { add, subtract, mul, divide };
